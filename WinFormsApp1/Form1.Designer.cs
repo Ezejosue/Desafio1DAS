d03ab47d0bff02b11071b213fb980a85322532de
SofLR <sofia.rojas2890@gmail.com>
2023-09-07 19:34:39 -0600

    ESTILO A FORM 1 Y A LOGING
    Se agregaron  etiquetas , imagenes, fondos y botones , amaño de formulario

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsApp1
 {
-    partial class Form1
+    partial class FormWelcome
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,32 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            timerWelcome = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            button1.Location = new Point(261, 113);
-            button1.Name = "button1";
-            button1.Size = new Size(210, 51);
-            button1.TabIndex = 0;
-            button1.Text = "sofia1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            panel1.BackColor = Color.FromArgb(28, 11, 43);
+            panel1.Location = new Point(-6, -5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(834, 125);
+            panel1.TabIndex = 1;
             // 
-            // Form1
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.bienvenido;
+            pictureBox1.Location = new Point(293, 278);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(200, 200);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Arial Rounded MT Bold", 48F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(97, 145);
+            label1.Name = "label1";
+            label1.Size = new Size(628, 95);
+            label1.TabIndex = 3;
+            label1.Text = "BIENVENIDOS ";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.DarkOrchid;
+            label2.Location = new Point(264, 555);
+            label2.Name = "label2";
+            label2.Size = new Size(264, 27);
+            label2.TabIndex = 4;
+            label2.Text = "CARGANDO RECURSOS ....";
+            // 
+            // timerWelcome
+            // 
+            timerWelcome.Interval = 15000;
+            timerWelcome.Tick += timerwELCOM;
+            // 
+            // FormWelcome
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Name = "Form1";
+            BackColor = Color.FromArgb(48, 28, 65);
+            ClientSize = new Size(829, 697);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormWelcome";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private System.Windows.Forms.Timer timerWelcome;
     }
 }
--- WinFormsApp1/Login.Designer.cs
+++ WinFormsApp1/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsApp1
 {
-    partial class Login
+    partial class FormLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Login";
+            labLogin = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            labUsuario = new Label();
+            labClave = new Label();
+            btnSesion = new Button();
+            SuspendLayout();
+            // 
+            // labLogin
+            // 
+            labLogin.AutoSize = true;
+            labLogin.BackColor = SystemColors.HighlightText;
+            labLogin.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            labLogin.ForeColor = Color.FromArgb(128, 128, 255);
+            labLogin.Location = new Point(257, 39);
+            labLogin.Name = "labLogin";
+            labLogin.Size = new Size(271, 35);
+            labLogin.TabIndex = 2;
+            labLogin.Text = "INICIAR SECCION";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ActiveCaption;
+            textBox1.Location = new Point(408, 322);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(333, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.ActiveCaption;
+            textBox2.Location = new Point(408, 393);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(333, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // labUsuario
+            // 
+            labUsuario.AutoSize = true;
+            labUsuario.BackColor = SystemColors.HighlightText;
+            labUsuario.Font = new Font("Arial", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            labUsuario.ForeColor = Color.FromArgb(128, 128, 255);
+            labUsuario.Location = new Point(233, 322);
+            labUsuario.Name = "labUsuario";
+            labUsuario.Size = new Size(118, 27);
+            labUsuario.TabIndex = 5;
+            labUsuario.Text = "USUARIO";
+            // 
+            // labClave
+            // 
+            labClave.AutoSize = true;
+            labClave.BackColor = SystemColors.HighlightText;
+            labClave.Font = new Font("Arial", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            labClave.ForeColor = Color.FromArgb(128, 128, 255);
+            labClave.Location = new Point(233, 393);
+            labClave.Name = "labClave";
+            labClave.Size = new Size(88, 27);
+            labClave.TabIndex = 6;
+            labClave.Text = "CLAVE";
+            // 
+            // btnSesion
+            // 
+            btnSesion.Font = new Font("Arial Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSesion.ForeColor = Color.FromArgb(128, 128, 255);
+            btnSesion.Location = new Point(475, 479);
+            btnSesion.Margin = new Padding(0, 3, 0, 2);
+            btnSesion.Name = "btnSesion";
+            btnSesion.Size = new Size(266, 33);
+            btnSesion.TabIndex = 7;
+            btnSesion.Text = "INICIAR SESION ";
+            btnSesion.UseVisualStyleBackColor = true;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.usuario;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(811, 650);
+            Controls.Add(btnSesion);
+            Controls.Add(labClave);
+            Controls.Add(labUsuario);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(labLogin);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormLogin";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label labLogin;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label labUsuario;
+        private Label labClave;
+        private Button btnSesion;
     }
 }
