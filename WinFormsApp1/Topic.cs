2e65bd1f356cad32b87631d1356cec652c0225c3
KevinEnriqueMartinezMartinez <127909032+KevinEnriqueMartinezMartinez@users.noreply.github.com>
2023-09-09 12:24:38 -0600

    Se agregaron las clases
    Se agrego la clase User y Clase topic para darle estructura al proyecto.

---
+++ WinFormsApp1/Topic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Topic
+    {
+        public String logo { get; set; }
+
+        public String Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public List<String> Country { get; set; }
+    }
+}
---
+++ WinFormsApp1/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class User
+    {
+        public String UserName { get; }
+
+        public String Password { get; }
+
+        public String Theme { get; }
+
+        public User(String userName, String password, String theme)
+        {
+            UserName = userName;
+            Password = password;
+            Theme = theme;
+
+        }
+
+    }
+}
